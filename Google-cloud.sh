9a34d33b814e85c2113179549727189f5ed93ccd
Faizan Mahmood <faizanworkmail1@gmail.com>
2025-01-25 19:05:34 +0500

    Create Google-cloud.sh

---
+++ Google-cloud.sh
@@ -0,0 +1,4 @@
+
+gcloud storage cp gs://cloud-training/bdml/taxisrcdata/schema.json  gs://qwiklabs-gcp-01-147a63a847c2-bucket/tmp/schema.json
+gcloud storage cp gs://cloud-training/bdml/taxisrcdata/transform.js  gs://qwiklabs-gcp-01-147a63a847c2-bucket/tmp/transform.js
+gcloud storage cp gs://cloud-training/bdml/taxisrcdata/rt_taxidata.csv  gs://qwiklabs-gcp-01-147a63a847c2-bucket/tmp/rt_taxidata.csv
