5c5cc01e1cc56ff927993ccacbfa72da0400128b
Faizan Mahmood <faizanworkmail1@gmail.com>
2025-01-27 16:54:18 +0500

    Update Googlecloud.sh

--- Googlecloud.sh
+++ Googlecloud.sh
@@ -7,3 +7,12 @@ gcloud storage cp gs://cloud-training/bdml/taxisrcdata/rt_taxidata.csv  gs://qwi
 gcloud services disable dataflow.googleapis.com
 gcloud services enable dataflow.googleapis.com
 
+
+
+# Ingest a new dataset from Google Cloud Storage (--sricpt--shell)
+bq load \
+--source_format=CSV \
+--autodetect \
+--noreplace  \
+nyctaxi.2018trips \
+gs://cloud-training/OCBL013/nyc_tlc_yellow_trips_2018_subset_2.csv
